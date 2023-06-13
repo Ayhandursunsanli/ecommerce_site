53811fb69f8a45ce56cc6461e18ce8483c3b665f
Ayhandursunsanli <ayhandursunsanli@gmail.com>
2023-06-13 20:10:49 +0300

    add

--- Backend/eticaret/productsapp/admin.py
+++ Backend/eticaret/productsapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Slogan
+
 
 # Register your models here.
+
+
+admin.site.register(Slogan)
--- Backend/eticaret/productsapp/models.py
+++ Backend/eticaret/productsapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Slogan(models.Model):
+    gorsel = models.ImageField(upload_to='headerphoto/', blank=False, null=False)
+    miniBaslik = models.CharField(max_length=100)
+    buyukBaslik = models.CharField(max_length=100)
+    aciklama = models.CharField(max_length=100)
+
