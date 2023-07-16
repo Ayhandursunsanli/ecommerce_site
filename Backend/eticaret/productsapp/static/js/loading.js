c6a15a25ea5fec54a8b8089a507d130687b8f2aa
Ayhandursunsanli <ayhandursunsanli@gmail.com>
2023-07-16 22:29:52 +0300

    add

--- Backend/eticaret/productsapp/static/js/loading.js
+++ Backend/eticaret/productsapp/static/js/loading.js
@@ -1,27 +1,27 @@
 // 3 saniye boyunca loading ekranı için -- ya bu seçilecek
 
-window.addEventListener('DOMContentLoaded', function() {
-    var loadingScreen = document.getElementById('loading-screen');
-    loadingScreen.style.display = 'flex';
-});
-
-// Tüm sayfa içeriği yüklendikten sonra loading ekranını gizle
-window.addEventListener('load', function() {
-    setTimeout(function() {
-        var loadingScreen = document.getElementById('loading-screen');
-        loadingScreen.style.display = 'none';
-    }, 1000);
-});
-
-//ekran ne zaman yüklenirse o zaman kaybolması için -- ya bu seçilecek
-
 // window.addEventListener('DOMContentLoaded', function() {
 //     var loadingScreen = document.getElementById('loading-screen');
 //     loadingScreen.style.display = 'flex';
 // });
 
-// // Tüm sayfa içeriği yüklendiğinde loading ekranını gizle
+// // Tüm sayfa içeriği yüklendikten sonra loading ekranını gizle
 // window.addEventListener('load', function() {
-//     var loadingScreen = document.getElementById('loading-screen');
-//     loadingScreen.style.display = 'none';
+//     setTimeout(function() {
+//         var loadingScreen = document.getElementById('loading-screen');
+//         loadingScreen.style.display = 'none';
+//     }, 1000);
 // });
+
+//ekran ne zaman yüklenirse o zaman kaybolması için -- ya bu seçilecek
+
+window.addEventListener('DOMContentLoaded', function() {
+    var loadingScreen = document.getElementById('loading-screen');
+    loadingScreen.style.display = 'flex';
+});
+
+// Tüm sayfa içeriği yüklendiğinde loading ekranını gizle
+window.addEventListener('load', function() {
+    var loadingScreen = document.getElementById('loading-screen');
+    loadingScreen.style.display = 'none';
+});
