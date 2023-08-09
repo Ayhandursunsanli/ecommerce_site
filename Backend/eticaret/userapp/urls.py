9751e6a6f2a5c992fd8a251e4abb95533d337552
Ayhandursunsanli <ayhandursunsanli@gmail.com>
2023-08-09 12:36:18 +0300

    teslimat to productsapp

--- Backend/eticaret/userapp/urls.py
+++ Backend/eticaret/userapp/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path('logout/',logout_request,name='logout'),
     path('update_profile/', update_profile, name='update_profile'),
     path('new_password/', new_password, name='new_password'),
-    path('teslimat/', teslimat, name='teslimat'),
     path('activate/<str:uidb64>/<str:token>/', activate, name='activate'),
 ]
