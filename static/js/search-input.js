c3ce2297d6b8f13ff310c5e9fafed24fda856886
Ayhandursunsanli <ayhandursunsanli@gmail.com>
2023-08-28 10:15:53 +0300

    js ile search yapıldı
    Kullanılmak istenirse kodun altına nasıl kullanılacağı not edildi. Şimdilik django ile kullanılacak

---
+++ static/js/search-input.js
@@ -0,0 +1,36 @@
+
+
+// const searchInput = document.getElementById('searchInput');
+// const productCards = document.querySelectorAll('.col-xl-4');
+
+// searchInput.addEventListener('input', () => {
+//     const searchTerm = searchInput.value.trim().toLowerCase();
+
+//     productCards.forEach(card => {
+//         const productName = card.querySelector('.name').textContent.toLowerCase();
+//         const productCategory = card.querySelector('.kategori')?.textContent.toLowerCase();
+//         const productStockCode = card.querySelector('.stokKodu')?.textContent.toLowerCase();
+//         const productColor = card.querySelector('.urunRengi')?.textContent.toLowerCase();
+//         const productFootCover = card.querySelector('.ayakKaplama')?.textContent.toLowerCase();
+//         const productDescription = card.querySelector('.aciklama')?.textContent.toLowerCase();
+
+//         const matches =
+//             productName.includes(searchTerm) ||
+//             (productCategory && productCategory.includes(searchTerm)) ||
+//             (productStockCode && productStockCode.includes(searchTerm)) ||
+//             (productColor && productColor.includes(searchTerm)) ||
+//             (productFootCover && productFootCover.includes(searchTerm)) ||
+//             (productDescription && productDescription.includes(searchTerm));
+
+//         card.style.display = matches ? 'block' : 'none';
+//     });
+// });
+
+
+//* Eğer js ile arama etkinleştirilirse html de form içinde bulunan arama butonu, form yerine dive çevrilir token kaldırılır get isteği iptal edilir ve sayfanın yenilenmesi önlenir. htmlde yukarıdaki değişkenleri çağırabilmek için d-none olan modelden parametreler çağırdım. Kullanılmazsa geri silinebilir.
+
+
+
+
+
+
